11e90231682ed084043bcf50e13ca92fb572d7b7
Luis Quispe Rios <luisqrja@gmail.com>
2021-09-05 19:56:09 -0500

    styles: put sidebar styles and grid areas for layout

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <PxHaeader />
-  <router-view />
+  <PxSideBarCategories />
+  <router-view class="view" />
   <PxFooter />
 </template>
 
 <script>
 import PxHaeader from "@/components/Header/PxHaeader";
 import PxFooter from "@/components/Footer/PxFooter";
+import PxSideBarCategories from "@/components/SideBarCategories/PxSideBarCategories";
 
 export default {
   components: {
     PxHaeader,
     PxFooter,
+    PxSideBarCategories,
   },
 };
 </script>
 
 <style lang="scss">
 @import "~@/assets/scss/_base.scss";
+@import "~@/assets/scss/App.scss";
 </style>
---
+++ src/components/SideBarCategories/PxSideBarCategories.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="sidebar">
+    <h2>Categorias</h2>
+    <ul class="siderbar__list">
+      <li class="sidebar__item">
+        <router-link class="sidebar__link" to="/">
+          Categorias 1
+          <font-awesome-icon icon="chevron-right" />
+        </router-link>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+//Icons
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faChevronRight);
+
+export default {
+  name: "PxSideBarCategories",
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/components/_PxSideBarCategories.scss";
+</style>
